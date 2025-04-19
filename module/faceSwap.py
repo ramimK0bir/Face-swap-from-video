792f6a4e7f51e20a510b73fdf58ca67d7abc37ac
ramimK0bir <ramim1000r@gmail.com>
2025-04-19 19:59:12 +0600

    Update faceSwap.py

--- module/faceSwap.py
+++ module/faceSwap.py
@@ -163,31 +163,51 @@ class videoEditor :
 
 
 
-    def addAudioToVideo(self, video_path, audio_source_path, output_path,
-                        crf=23, preset="medium", audio_codec="aac", strict="experimental"):
+    # def addAudioToVideo(self, video_path, audio_source_path, output_path,
+    #                     crf=23, preset="medium", audio_codec="aac", strict="experimental"):
+    #     try:
+    #         command = [
+    #             'ffmpeg',
+    #             '-y',  # Overwrite output
+    #             '-i', video_path,
+    #             '-i', audio_source_path,
+    #             '-c:v', 'libx264',         # Compress video
+    #             '-crf', str(crf),          # Quality setting
+    #             '-preset', preset,         # Compression speed
+    #             '-c:a', audio_codec,       # Audio codec
+    #             '-strict', strict,         # Compatibility flag
+    #             output_path
+    #         ]
+    
+    #         self.subprocess.run(command, check=True)
+    #         print(f"\nSuccessfully added audio and compressed video. Output saved at '{output_path}'.")
+    
+    #     except self.subprocess.CalledProcessError as e:
+    #         raise ValueError(f"FFmpeg processing failed: {e}. \nVideo saved as {video_path} without any audio.")
+    #     except Exception as e:
+    #         raise ValueError(f"Unexpected error: {e}")
+    def addAudioToVideo(self, video_path, audio_source_path, output_path, video_codec="copy", audio_codec="aac", strict="experimental"):
         try:
             command = [
                 'ffmpeg',
-                '-y',  # Overwrite output
+                '-y',  # force overwrite
                 '-i', video_path,
                 '-i', audio_source_path,
-                '-c:v', 'libx264',         # Compress video
-                '-crf', str(crf),          # Quality setting
-                '-preset', preset,         # Compression speed
-                '-c:a', audio_codec,       # Audio codec
-                '-strict', strict,         # Compatibility flag
+                '-c:v', video_codec,
+                '-c:a', audio_codec,
+                '-strict', strict,
                 output_path
             ]
-    
+
             self.subprocess.run(command, check=True)
-            print(f"\nSuccessfully added audio and compressed video. Output saved at '{output_path}'.")
-    
+            print(f"\nSuccessfully added audio from '{audio_source_path}' to video '{video_path}'. Output saved at '{output_path}'.")
         except self.subprocess.CalledProcessError as e:
-            raise ValueError(f"FFmpeg processing failed: {e}. \nVideo saved as {video_path} without any audio.")
+            raise ValueError(f"FFmpeg processing failed: {e}. \nvideo saved as {video_path} withut any audio.")
         except Exception as e:
             raise ValueError(f"Unexpected error: {e}")
 
 
+    
                         
 
     def cleanup(self,path :str):
